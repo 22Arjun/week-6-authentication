ff6ef661e7fc699b8bcfcca5b10038184dd4c71e
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-11-26 17:55:43 +0530

    added frontend

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
+const axios = require('axios');
 
 const JWT_SECRET = "JaiShreeKrishna";
 
 let users = [];
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
 
 
 app.post('/signup', (req, res) => {
@@ -15,7 +21,7 @@ app.post('/signup', (req, res) => {
     const user = users.find(u => u.username == username);
     if(user){
          res.status(403).send({
-            message : 'you are already logged in. Please Sign In now.'
+            message : 'you are already signed up. Please Sign In now.'
         })
     }
     else {
@@ -39,66 +45,91 @@ app.post('/signin', (req, res) => {
     const password = req.body.password;
 
     
-    const user = users.find(u => u.username == username && password == password);
-   
-    
-    if(user) {
-        
-        const token = jwt.sign({
-            username: user.username
-        }, JWT_SECRET);
-        
-        user.token = token;
-
-        res.header("token", token);
+    let foundUser = null;
+    for(let i = 0; i < users.length; i++) {
+        if(users[i].username == username && users[i].password == password) {
+           foundUser = users[i];
+        }
     }
-    else {
+
+    if(!foundUser) {
         res.status(403).send({
             message: "Invalid Username or Password"
-    });
+        });
+    }
+    else {
+        const token = jwt.sign({
+            username : foundUser.username
+        }, JWT_SECRET);
+
+        res.header("token", token);
+        foundUser.token = token;
+        
+        res.send({
+            token: token
+        });
     }
 })
 
 
 const auth = (req, res, next) => {
     const token = req.headers.token;
-    const decodedData = jwt.verify(token, JWT_SECRET);
-    const user = users.find(u => u.username == decodedData.username);
-    if(user.token == token) {
+
+    const decodedUser = jwt.verify(token, JWT_SECRET);
+
+    if(decodedUser.username) {
+        req.username = decodedUser.username;
         next();
+
     }
     else {
-        res.send({
-            message: "You're not logged in, let's log in first"
+        res.status(403).json({
+            error: "You are not signed in, Please sign in."
         })
     }
+    
 }
 
 
 
 app.post('/me', auth, (req, res) => {
-    const token = req.headers.token;
+    const currentUser = req.username;
+   let foundUser = null;
 
-    const decodedData = jwt.verify(token, JWT_SECRET);
+   for (let i = 0; i < users.length; i++) {
+        if(users[i].username == currentUser) {
+            foundUser = users[i];
+        }
+   }
 
-    if(decodedData) {
-        const user = users.find(u => u.username == decodedData.username);
-        if(user) {
-            res.send({
-                username: user.username,
-                password: user.password
-            })
+   if(!foundUser) {
+        res.status(403).json({
+            message: "Something went wrong!"
         }
-        else {
-            res.status(403).send({
-                error: "You're not signed in"
-            })
-        }
-    }
-    else {
-        res.status(403).send({
-            error: 'you are a pirate, Get off my website now!'
-        })
-    }
+        );
+   }
+   else {
+    res.send({
+                username: foundUser.username,
+                password: foundUser.password
+            });
+   };
+
+}
+);
+
+   //GET Requests
+
+app.get('/signup', (req, res) => {
+    res.send({
+        users: users
+    })
 });
+
+
+            
+
+        
+
+
 app.listen(3000);
